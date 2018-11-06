c7649bec58a035b5fbc595ffb8acb5e1f31f9022
celtare21 <celtare21@gmail.com>
2018-11-06 20:15:51 +0000

    GCC 8 built from Nathan's script
    Signed-off-by: celtare21 <celtare21@gmail.com>

---
+++ lib/gcc/aarch64-linux-gnu/8.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --enable-languages=c --target=aarch64-linux-gnu --prefix=/home/vladutadumitroaie21/gcc/aarch64-linux-gnu --disable-multilib --disable-werror CFLAGS='-g0 -O2' CXXFLAGS='-g0 -O2'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
