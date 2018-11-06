264fc4cbe0a72cfe97291458432fb818ca007b53
celtare21 <celtare21@gmail.com>
2018-11-06 20:20:16 +0000

    Gcc 8 built from Nathan's script
    Signed-off-by: celtare21 <celtare21@gmail.com>

---
+++ lib/gcc/arm-linux-gnueabi/8.2.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --enable-languages=c --target=arm-linux-gnueabi --prefix=/home/vladutadumitroaie21/gcc/arm-linux-gnueabi --disable-multilib --disable-werror CFLAGS='-g0 -O2' CXXFLAGS='-g0 -O2'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "tls", "gnu" } };
